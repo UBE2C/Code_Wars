a180d52c5045478d12ced6044e85e13579758dce
UBE2C <gabor.bakos01@gmail.com>
2025-09-09 23:09:49 +0200

    I finished the first challange from the RoboScript series - syntax highlighting.

--- Code_Wars.py
+++ Code_Wars.py
@@ -2021,3 +2021,87 @@ def assembler_interpreter(program):
 ###################################################################################################  Kata end  #####################################################################################################
 
 
+
+
+
+
+###################################################################################################     Esolang Interpreters #8     #####################################################################################################
+                                                                                                  #   RoboScript series 1 - 6 kyu   #
+
+"""
+Your MyRobot-specific (esoteric) scripting language called RoboScript only ever contains the following characters: F, L, R, the digits 0-9 and brackets (( and )). Your goal is to write a function highlight which accepts 1 required argument code which is the RoboScript program passed in as a string and returns the script with syntax highlighting. The following commands/characters should have the following colors:
+
+F - Wrap this command around <span style="color: pink"> and </span> tags so that it is highlighted pink in our editor
+L - Wrap this command around <span style="color: red"> and </span> tags so that it is highlighted red in our editor
+R - Wrap this command around <span style="color: green"> and </span> tags so that it is highlighted green in our editor
+Digits from 0 through 9 - Wrap these around <span style="color: orange"> and </span> tags so that they are highlighted orange in our editor
+Round Brackets - Do not apply any syntax highlighting to these characters
+For example:
+
+highlight("F3RF5LF7"); // => "<span style=\"color: pink\">F</span><span style=\"color: orange\">3</span><span style=\"color: green\">R</span><span style=\"color: pink\">F</span><span style=\"color: orange\">5</span><span style=\"color: red\">L</span><span style=\"color: pink\">F</span><span style=\"color: orange\">7</span>"
+And for multiple characters with the same color, simply wrap them with a single <span> tag of the correct color:
+
+highlight("FFFR345F2LL"); // => "<span style=\"color: pink\">FFF</span><span style=\"color: green\">R</span><span style=\"color: orange\">345</span><span style=\"color: pink\">F</span><span style=\"color: orange\">2</span><span style=\"color: red\">LL</span>"
+Note that the use of <span> tags must be exactly the same format as demonstrated above. Even if your solution produces the same visual result as the expected answers, if you miss a space betwen "color:" and "green", for example, you will fail the tests.
+"""
+
+def highlight(code: str) -> str:
+    return_string: str = ""
+
+    char_index: int = 0
+    while char_index < len(code):
+        if code[char_index] == "F":
+            return_string += '<span style="color: pink">'
+            
+            sub_index: int = char_index
+            while sub_index < len(code) and code[sub_index] == "F":
+                return_string += f"{code[sub_index]}"
+                sub_index += 1
+                char_index = sub_index - 1
+            
+            return_string += "</span>"
+
+        elif code[char_index] == "L":
+            return_string += '<span style="color: red">'
+
+            sub_index: int = char_index
+            while sub_index < len(code) and code[sub_index] == "L":
+                return_string += f"{code[sub_index]}"
+                sub_index += 1
+                char_index = sub_index - 1
+
+            return_string += "</span>"
+
+        elif code[char_index] == "R":
+            return_string += '<span style="color: green">'
+
+            sub_index: int = char_index
+            while sub_index < len(code) and code[sub_index] == "R":
+                return_string += f"{code[sub_index]}"
+                sub_index += 1
+                char_index = sub_index - 1
+
+            return_string += "</span>"
+
+        elif code[char_index].isdigit():
+            return_string += '<span style="color: orange">'
+
+            sub_index: int = char_index
+            while sub_index < len(code) and code[sub_index].isdigit():
+                return_string += f"{code[sub_index]}"
+                sub_index += 1
+                char_index = sub_index - 1
+
+            return_string += "</span>"
+
+        elif code[char_index] == "(" or code[char_index] == ")":
+            return_string += code[char_index]
+
+        else:
+            raise ValueError(f"highlight: unexpected character {code[char_index]} at position {char_index}.")
+        
+        char_index += 1
+        
+    return return_string
+
+###################################################################################################     Esolang Interpreters #8     #####################################################################################################
